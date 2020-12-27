0f6aece2ff522e3572f7c89d0babc25f3ef1b018
Las <crowclaw935@gmail.com>
2020-12-28 01:09:06 +0300

    scroll added

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,17 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  title(String title) {
+    return Row(
+      children: [
+        Text(title,
+            style: TextStyle(color: Colors.blue.shade800, fontSize: 25)),
+        Spacer(),
+        Text("more"),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,10 +37,58 @@ class Home extends StatelessWidget {
           IconButton(icon: Icon(Icons.search)),
         ],
       ),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
           child: Column(
-        children: [],
-      )),
+            children: [
+              title('future'),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                  height: 200,
+                  child: ListView(
+                    children: [cards(), cards(), cards(), cards(), cards()],
+                    scrollDirection: Axis.horizontal,
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class cards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 100,
+      child: Card(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/image1.jpg'))),
+            ),
+            Container(
+              child: Text("world wonders"),
+            ),
+            Container(
+              child: Text("world wonders"),
+            ),
+            Container(
+              child: Text("world wonders"),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
